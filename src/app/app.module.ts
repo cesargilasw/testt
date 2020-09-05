4ab19299ff5f294c25574e7b42bf50aca45b740f
cesarsc333 <cscvr33@gmail.com>
2020-09-05 14:57:15 -0700

    Add missing components to shared modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
-import { HomeComponent } from './pages/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule} from "@angular/common/http";
 
 // Angular Material
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -14,25 +11,19 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSortModule } from '@angular/material/sort';
 
-// FontAwesomeModule
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
+import { SharedModule } from './shared/shared.module';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    HomeComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
     BrowserAnimationsModule,
-    FormsModule,
-    ReactiveFormsModule,
+    SharedModule,
     MatSnackBarModule,
     MatProgressSpinnerModule,
-    FontAwesomeModule,
     MatIconModule,
     MatSortModule
   ],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedRoutingModule } from './shared-routing.module';
+import { HttpClientModule} from "@angular/common/http";
 import { NavbarComponent } from '../components/navbar/navbar.component';
+import { HomeComponent } from '../pages/home/home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, HomeComponent],
   imports: [
     CommonModule,
-    SharedRoutingModule
+    SharedRoutingModule,
+    FontAwesomeModule
   ],
   exports:[
-    NavbarComponent
+    NavbarComponent,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FontAwesomeModule
   ]
 })
 export class SharedModule { }
